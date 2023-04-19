6ee50123e515b880e421972f77df0e731b34255b
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-04-19 12:03:54 +0100

    promise

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,11 +2,9 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    const respon_uploadPhoto = await uploadPhoto();
-    const respon_createUser = await createUser();
     return {
-      photo: respon_uploadPhoto,
-      user: respon_createUser,
+      photo: await uploadPhoto(),
+      user: await createUser(),
     };
   } catch (_) {
     return { photo: null, user: null };
