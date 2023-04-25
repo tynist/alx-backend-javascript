34f3b3c1b5ed6202b5570e9e53cdc1abb6638b3d
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-04-25 20:00:33 +0100

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,13 @@
 export default function getListStudents() {
-  const createStudent = (id, firstName, location) => ({
+  const makeStudent = (id, firstName, location) => ({
     id,
     firstName,
     location,
   });
 
   return [
-    createStudent(1, 'Guillaume', 'San Francisco'),
-    createStudent(2, 'James', 'Columbia'),
-    createStudent(5, 'Serena', 'San Francisco'),
+    makeStudent(1, 'Guillaume', 'San Francisco'),
+    makeStudent(2, 'James', 'Columbia'),
+    makeStudent(5, 'Serena', 'San Francisco'),
   ];
 }
