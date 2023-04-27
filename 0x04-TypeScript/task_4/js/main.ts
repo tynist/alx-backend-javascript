8547e3c6f536f8397febaf5a0089dc7caa8b2b35
Peter Ughasi <ximonak@gmail.com>
2023-04-27 15:06:59 +0000

    typescript

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,11 @@
+namespace Subject {
+  export const cpp = Cpp;
+  export const java = Java;
+  export const react = React;
+
+  export const teacher: Teacher = {
+    name: "test",
+    subject: "test",
+    yearsOfExperience: 11
+  }
+}
