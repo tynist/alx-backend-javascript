6268b0efd07f3dee4117102f151106ff96b96ec3
Peter <ximonaka@gmail.com>
2023-06-12 08:28:05 +0000

    using process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+// Display the welcome message to STDOUT.
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// Listen for the user input.
+process.stdin.on("data", (input) => {
+  // Get the user's name from STDIN.
+  const name = input; // Assign the user input to the "name" variable
+
+  // Display the user's name to STDOUT.
+  process.stdout.write(`Your name is: ${name}\n`);
+});
+
+// Display closing message when user ends the program.
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
+});
