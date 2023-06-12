03e99e17eb4b72abe95ded4bed5cc20655a4f944
Peter <ximonaka@gmail.com>
2023-06-12 14:06:58 +0000

    more complex HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  // Set the response header to plain text
+  res.setHeader('Content-Type', 'text/plain');
+
+  // Handle requests based on the URL path
+  if (req.url === '/') {
+    // Display "Hello Holberton School!"
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    // If the URL path is "/students", use countStudents function with the database file
+    const databaseFile = process.argv[2];
+
+    // Call the countStudents function to get the list of students
+    countStudents(databaseFile)
+      .then((result) => {
+        // Display the result (list of students)
+        res.statusCode = 200;
+        res.end(`This is the list of our students\n${result}`);
+      })
+      .catch((error) => {
+        // Display an error message
+        res.statusCode = 500;
+        res.end(`Error: ${error.message}`);
+      });
+  } else {
+    // Display a 404 Not Found error
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+// Listen on port 1245
+app.listen(1245);
+
+// Export the app variable
+module.exports = app;
