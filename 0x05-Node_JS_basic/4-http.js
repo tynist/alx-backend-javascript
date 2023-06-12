e71577e2053caa889a9a97fd37bce205f648388b
Peter <ximonaka@gmail.com>
2023-06-12 13:19:52 +0000

    small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,10 +3,10 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   // Set the response status code to 200
   res.statusCode = 200;
-  
+
   // Set the response content type to text/plain
   res.setHeader('Content-Type', 'text/plain');
-  
+
   //  Send the response body
   res.end('Hello Holberton School!');
 });
