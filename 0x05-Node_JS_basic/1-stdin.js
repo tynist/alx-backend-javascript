017a787fcf0ae00853c4c20c2b7542ebefe05760
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-12 11:20:50 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,15 +3,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Listen for the user input.
 process.stdin.on('data', (userInput) => {
-  const userInput = process.stdin.read(); // Read user input from STDIN
-
-  // Check if user input is not null & Display the user's name to STDOUT
-  if (userInput !== null) {
-    process.stdout.write(`Your name is: ${userInput}`);
-  }
+  // Read user input from STDIN and display the user's name to STDOUT
+  process.stdout.write(`Your name is: ${userInput}`);
 });
 
-// Display the closing message when user ends the program.
+// Display the closing message when the user ends the program.
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
