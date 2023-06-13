8aecb33d6c80341d83ea09545830b71cf9fdd56f
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-13 12:10:06 +0100

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,15 +2,16 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-const FILE = process.argv[2];
+const port = 1245;
+const databaseFile = process.argv[2];
 
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (_, res) => {
-  countStudents(FILE)
-    .then((data) => res.send(`This is the list of our students\n${data}`))
+app.get('/students', (req, res) => {
+  countStudents(databaseFile)
+    .then((studentData) => res.send(`This is the list of our students\n${studentData}`))
     .catch(() => {
       res.write('This is the list of our students\n');
       res.write('Cannot load the database');
