8ad553d386c529491c25017de4f10c1075302af6
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-13 12:20:39 +0100

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,13 +5,20 @@ const app = express();
 const port = 1245;
 const databaseFile = process.argv[2];
 
+// Define a route for the endpoint '/' URL
 app.get('/', (req, res) => {
+  // Send the response
   res.send('Hello Holberton School!');
 });
 
+// Define a route for the endpoint '/students'
 app.get('/students', (req, res) => {
-  countStudents(databaseFile)
-    .then((studentData) => res.send(`This is the list of our students\n${studentData}`))
+  countStudents(databaseFile)  // Call function to get d list of students
+    .then((studentData) => {
+      // Display the studentData (list of students)
+      res.send(`This is the list of our students\n${studentData}`);
+    })
+    // If error occurs, display error message
     .catch(() => {
       res.write('This is the list of our students\n');
       res.write('Cannot load the database');
@@ -19,6 +26,8 @@ app.get('/students', (req, res) => {
     });
 });
 
-app.listen(1245);
+//Listen on port 1245
+app.listen(port);
 
+// Export the app variable
 module.exports = app;
