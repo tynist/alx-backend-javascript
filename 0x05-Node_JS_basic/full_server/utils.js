c95d5befc59ee3ad391e9306d0f2a58737924e62
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-13 13:54:02 +0100

    Update utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,22 +1,39 @@
 import fs from 'fs';
+import util from 'util';
 
-export function readDatabase(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const lines = data.trim().split('\n');
-        const fields = {};
-        lines.forEach((line) => {
-          const [firstName, field] = line.split(',');
-          if (!fields[field]) {
-            fields[field] = [];
+const readFile = util.promisify(fs.readFile);
+
+export default function readDatabase(filePath) {
+  return readFile(filePath, 'utf-8')
+    .then((data) => {
+      const records = data.split('\n');
+      const csMajor = {
+        name: 'CS',
+        students: [],
+      };
+      const sweMajor = {
+        name: 'SWE',
+        students: [],
+      };
+      const fields = [csMajor, sweMajor];
+
+      for (const index in records) {
+        if (index > 0) {
+          const [student, , , field] = records[index].split(',');
+          if (field === 'CS') {
+            csMajor.students.push(` ${student}`);
+          } else if (field === 'SWE') {
+            sweMajor.students.push(` ${student}`);
           }
-          fields[field].push(firstName);
-        });
-        resolve(fields);
+        }
       }
+
+      fields[0].students[0] = fields[0].students[0].trim();
+      fields[1].students[0] = fields[1].students[0].trim();
+
+      return fields;
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
     });
-  });
 }
