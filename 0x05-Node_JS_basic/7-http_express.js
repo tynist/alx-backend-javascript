ad47157c877ef829c540af4d6441ad380d815509
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-13 11:23:53 +0100

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,39 +2,32 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
+const port = 1245;
+const databaseFile = process.argv[2];
 
 // Define a route for the endpoint '/students'
 app.get('/students', (req, res) => {
-  // Set the response header to plain text
-  res.set('Content-Type', 'text/plain');
-
-  // Retrieve the database file path from command-line arguments
-  const databaseFile = process.argv[2];
-
-  // Call the countStudents function to get the list of students
-  countStudents(databaseFile)
+  countStudents(databaseFile) // Call function to get d list of students
     .then((studentData) => {
       // Display the studentData (list of students)
-      const resp = `This is the list of our students\n${studentData}`;
-      res.send(resp);
+      const response = `This is the list of our students\n${studentData}`;
+      res.send(response);
     })
     .catch(() => {
       // If error occurs during function call, display error message
-      res.statusCode = 500; // Internal Server Error
-      res.write('This is the list of our students\n');
-      res.write('Cannot load the database');
-      res.end();
+      const resp = 'This is the list of our students\nCannot load the database';
+      res.status(500).send(resp;
     });
 });
 
 // Define a route for the endpoint '/' URL
-app.get('/students', async (req, res) => {
+app.get('/', (req, res) => {
   // Handle the root URL
   res.send('Hello Holberton School');
 });
 
 // Listen on port 1245
-app.listen(1245);
+app.listen(port);
 
 // Export the app variable
 module.exports = app;
