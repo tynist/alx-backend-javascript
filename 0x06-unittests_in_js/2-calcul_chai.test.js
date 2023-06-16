9c0686aaa795393a65c0e18bcb0a8ef7b851714c
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-16 13:12:28 +0100

    Update 2-calcul_chai.test.js

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,75 +1,55 @@
-const { expect } = require('chai');
-const { describe, it } = require("mocha");
+const expect = require('chai').expect;
+const { describe, it } = require('mocha');
 const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
 
   // Test suite for SUM operation
   describe('SUM', () => {
-    // Test case: SUM operation 1
+    // Test case: checking if numbers round
     it('returns the sum of two rounded numbers', () => {
-      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+      expect(calculateNumber('SUM', 1, 2)).to.equal(3);
     });
-    // Test case: SUM operation 2
-    it('returns the sum of two rounded numbers', () => {
-      expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
-    });
-    // Test case: SUM operation 3
-    it("checking if numbers round", function() {
-      expect(calculateNumber("SUM", 1, 2)).to.equal(3);
-    });
-    // Test case: SUM operation 4
-    it("checking if numbers round", function() {
-      expect(calculateNumber("SUM", -1, -4.9)).to.equal(-5);
+
+    // Test case: checking if numbers round (decimal inputs)
+    it('returns the sum of two rounded numbers (decimal inputs)', () => {
+      expect(calculateNumber('SUM', 1.6, 2.6)).to.equal(5);
     });
   });
 
   // Test suite for SUBTRACT operation
   describe('SUBTRACT', () => {
-    // Test case: SUBTRACT operation 1
-    it('returns difference of two rounded numbers', () => {
-      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    // Test case: checking if numbers round
+    it('returns the difference of two rounded numbers', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 2.3)).to.equal(-1);
     });
-    // Test case: SUBTRACT operation 2
-    it('returns difference of two rounded numbers', () => {
-      expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
+    
+    // Test case: checking if numbers round (decimal inputs)
+    it('returns the difference of two rounded numbers (decimal inputs)', () => {
+      expect(calculateNumber('SUBTRACT', 4.9, 2.7)).to.equal(2);
     });
-    // Test case: SUBTRACT operation 3
-    it('returns difference of two rounded numbers', () => {
+    
+    // Test case: checking if numbers round (negative inputs)
+    it('returns the difference of two rounded numbers (negative inputs)', () => {
       expect(calculateNumber('SUBTRACT', -4.9, -2.7)).to.equal(-2);
-    });
-    // Test case: SUBTRACT operation 4
-    it('returns difference of two rounded numbers', () => {
-      expect(calculateNumber('SUBTRACT', -1.3, -3.7)).to.equal(3);
     });
   });
 
   // Test suite for DIVIDE operation
   describe('DIVIDE', () => {
-    // Test case: DIVIDE operation 1
-    it('returns division of two rounded numbers', () => {
-      expect(calculateNumber('DIVIDE', -1.4, -4.5)).to.equal(0.2);
+    // Test case: checking if numbers round
+    it('returns the division of two rounded numbers', () => {
+      expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
     });
-    // Test case: DIVIDE operation 2
-    it('returns division of two rounded numbers', () => {
-      expect(calculateNumber('DIVIDE', -2, 1)).to.equal(-2);
+    
+    // Test case: checking if numbers round (decimal inputs)
+    it('returns the division of two rounded numbers (decimal inputs)', () => {
+      expect(calculateNumber('DIVIDE', 4.6, 1.8)).to.equal(2.5);
     });
-    // Test case: DIVIDE operation 3
-    it('returns division of two rounded numbers', () => {
-      expect(calculateNumber('DIVIDE', 4, 3.7)).to.equal(1);
-    });
-
+    
     // Test case: returns "Error" for division by zero
     it('returns "Error" for division by zero', () => {
-      expect(calculateNumber('DIVIDE', 2.3, 0)).to.equal('Error');
-    });
-    // Test case: returns "Error" for an invalid type
-    it('returns "Error" for an invalid type', () => {
-      expect(calculateNumber(5, 1, 4)).to.equal('Error');
-    });
-    // Test case: returns "Error" for an invalid type
-    it('returns "Error" for an invalid type', () => {
-      expect(calculateNumber('plus', 1, 4)).to.equal('Error');
+      expect(calculateNumber('DIVIDE', 4, 0)).to.equal('Error');
     });
   });
 });
