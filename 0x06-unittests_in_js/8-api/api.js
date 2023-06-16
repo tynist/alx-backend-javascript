06c44d4d33418c05231f189a02ab1ae8400029c6
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-16 15:01:17 +0100

    Update api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,22 +1,12 @@
 const express = require('express');
 
 const app = express();
-const port = 7865; // Set the port number
+const port = 7865;
 
 app.listen(port, () => {
-  // Log the server availability
   console.log(`API available on localhost port ${port}`);
 });
 
 app.get('/', (req, res) => {
-  // Return a welcome message for the index page
   res.send('Welcome to the payment system');
 });
-
-app.get('/cart/:id(\\d+)', (req, res) => {
-  const { id } = req.params;
-  // Return payment methods for a specific cart
-  res.send(`Payment methods for cart ${id}`);
-});
-
-module.exports = app;
