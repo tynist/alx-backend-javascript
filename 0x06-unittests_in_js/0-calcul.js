72c4e9a5ec49f199631391162bf45759f5853905
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-16 10:07:00 +0100

    Update 0-calcul.js

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,10 @@
 function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
+  // Round the numbers
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+
+  // Return the sum of rounded numbers
+  return roundedA + roundedB;
 }
 
 module.exports = calculateNumber;
