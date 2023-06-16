b7504491ae44f5875fb95583369e7110eed9460d
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-16 08:01:13 +0100

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,10 +2,23 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-  it('should round the arguments and return the sum', () => {
+  it('should return the correct sum when both arguments are integers', () => {
+    // Test case 1: Both arguments are integers
     assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should round the arguments and return the correct sum when one argument is a decimal', () => {
+    // Test case 2: One argument is a decimal
     assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should round the arguments and return the correct sum when both arguments are decimals', () => {
+    // Test case 3: Both arguments are decimals
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should round the arguments and return the correct sum when both arguments are decimals', () => {
+    // Test case 4: Both arguments are decimals
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
