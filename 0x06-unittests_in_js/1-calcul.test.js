af3b62988a4125cd00b753ed93a842754146f67a
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-16 09:56:41 +0100

    Update 1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,25 +4,33 @@ const calculateNumber = require('./1-calcul');
 
 
 describe('calculateNumber', () => {
-  describe('SUM operation', () => {
-    it('returns the sum of two rounded numbers', () => {
-      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-    });
+  it('should return the sum of two rounded numbers for SUM operation', () => {
+    // Test case 1: SUM operation
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
   });
 
-  describe('SUBTRACT operation', () => {
-    it('returns the subtraction result of two rounded numbers', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-    });
+  it('should return the subtraction result for SUBTRACT operation', () => {
+    // Test case 2: SUBTRACT operation
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
   });
 
-  describe('DIVIDE operation', () => {
-    it('returns the division result of two rounded numbers', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-    });
+  it('should return the division result for DIVIDE operation', () => {
+    // Test case 3: DIVIDE operation
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
 
-    it('returns "Error" when the rounded divisor is zero', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-    });
+  it('should return "Error" for division by zero in DIVIDE operation', () => {
+    // Test case 4: DIVIDE operation with division by zero
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+
+  it('should return "Error" for invalid type in operation', () => {
+    // Test case 5: Checking if operation is correct
+    assert.strictEqual(calculateNumber(5, 1, 4), 'Error');
+  });
+
+  it('should return "Error" for unknown operation type', () => {
+    // Test case 6: Checking correct type for operation
+    assert.strictEqual(calculateNumber('plus', 1, 4), 'Error');
   });
 });
