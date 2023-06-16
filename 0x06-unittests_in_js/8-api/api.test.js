4803372db3e6d3961bd2c6fcec46be6a3a989277
Peter <ximonaka@gmail.com>
2023-06-16 12:57:35 +0000

    unittest in JS

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,30 @@
+const request = require('request');
+const { expect } = require('chai');
+
+// Tests index page of payment system
+describe('Index page', () => {
+  // Checks status code is 200 OK
+  it('Correct status code?', (done) => {
+    request.get('http://localhost:7865', (error, response) => {
+      expect(response.statusCode).toEqual(200);
+      done();
+    });
+  });
+
+  // Checks result is 'Welcome to the payment system'
+  it('Correct result?', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).toEqual('Welcome to the payment system');
+      done();
+    });
+  });
+
+  // Allows for additional assertions or tests
+  it('Other?', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      // Add any other assertions or tests you want to perform
+      done();
+    });
+  });
+});
+
