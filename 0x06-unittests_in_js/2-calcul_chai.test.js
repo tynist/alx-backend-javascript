35804bb3182124493676b9f8f6485361109e9acf
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-16 13:03:56 +0100

    Update 2-calcul_chai.test.js

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,13 +1,13 @@
 const { expect } = require('chai');
-const {describe, it} = require("mocha");
+const { describe, it } = require("mocha");
 const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
 
   // Test suite for SUM operation
   describe('SUM', () => {
-    it('returns the sum of two rounded numbers', () => {
     // Test case: SUM operation 1
+    it('returns the sum of two rounded numbers', () => {
       expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
     // Test case: SUM operation 2
@@ -16,12 +16,13 @@ describe('calculateNumber', () => {
     });
     // Test case: SUM operation 3
     it("checking if numbers round", function() {
-	    expect(calculateNumber("SUM", 1, 2)).to.equal(3);
+      expect(calculateNumber("SUM", 1, 2)).to.equal(3);
     });
     // Test case: SUM operation 4
     it("checking if numbers round", function() {
-	    expect(calculateNumber("SUM", -1, -4.9)).to.equal(-5);
+      expect(calculateNumber("SUM", -1, -4.9)).to.equal(-5);
     });
+  });
 
   // Test suite for SUBTRACT operation
   describe('SUBTRACT', () => {
@@ -33,28 +34,29 @@ describe('calculateNumber', () => {
     it('returns difference of two rounded numbers', () => {
       expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
     });
+    // Test case: SUBTRACT operation 3
     it('returns difference of two rounded numbers', () => {
-      // Test case: SUBTRACT operation 3
       expect(calculateNumber('SUBTRACT', -4.9, -2.7)).to.equal(-2);
     });
-    it("returns difference of two rounded numbers", () {
-       // Test case: SUBTRACT operation 4
-	    expect(calculateNumber("SUBTRACT", -1.3,-3.7)).to.equal(3);
+    // Test case: SUBTRACT operation 4
+    it('returns difference of two rounded numbers', () => {
+      expect(calculateNumber('SUBTRACT', -1.3, -3.7)).to.equal(3);
     });
+  });
 
   // Test suite for DIVIDE operation
   describe('DIVIDE', () => {
     // Test case: DIVIDE operation 1
-    it("returns division of two rounded numbers", () {
-	    expect(calculateNumber("DIVIDE", -1.4,-4.5)).to.equal(0.2);
+    it('returns division of two rounded numbers', () => {
+      expect(calculateNumber('DIVIDE', -1.4, -4.5)).to.equal(0.2);
     });
     // Test case: DIVIDE operation 2
-    it("returns division of two rounded numbers", () {
-	    expect(calculateNumber("DIVIDE", -2, 1)).to.equal(-2);
+    it('returns division of two rounded numbers', () => {
+      expect(calculateNumber('DIVIDE', -2, 1)).to.equal(-2);
     });
     // Test case: DIVIDE operation 3
-    it("returns division of two rounded numbers", () {
-	    expect(calculateNumber("DIVIDE", 4, 3.7)).to.equal(1);
+    it('returns division of two rounded numbers', () => {
+      expect(calculateNumber('DIVIDE', 4, 3.7)).to.equal(1);
     });
 
     // Test case: returns "Error" for division by zero
@@ -65,8 +67,9 @@ describe('calculateNumber', () => {
     it('returns "Error" for an invalid type', () => {
       expect(calculateNumber(5, 1, 4)).to.equal('Error');
     });
-
+    // Test case: returns "Error" for an invalid type
     it('returns "Error" for an invalid type', () => {
       expect(calculateNumber('plus', 1, 4)).to.equal('Error');
     });
+  });
 });
