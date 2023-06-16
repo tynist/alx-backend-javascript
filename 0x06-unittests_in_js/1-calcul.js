63718fe7dc1355fd8d3b7c3ce30bfa68703ad29c
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-16 10:05:59 +0100

    Update 1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,17 +1,26 @@
 function calculateNumber(type, a, b) {
+  // Round the numbers
   const roundedA = Math.round(a);
   const roundedB = Math.round(b);
 
+  // Check the type of operation
   if (type === 'SUM') {
+    // Return the sum of rounded numbers
     return roundedA + roundedB;
   } else if (type === 'SUBTRACT') {
+    // Return the subtraction of rounded numbers
     return roundedA - roundedB;
   } else if (type === 'DIVIDE') {
+    // Check if the rounded value of b is zero
     if (roundedB === 0) {
-      return 'Error';
+      return 'Error'; // Division by zero error
+    } else {
+      // Perform division and return the result
+      return roundedA / roundedB;
     }
-    return roundedA / roundedB;
   }
+
+  return 'Error'; // Invalid operation type
 }
 
 module.exports = calculateNumber;
