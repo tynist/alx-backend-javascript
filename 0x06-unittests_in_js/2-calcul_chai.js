772aa6ef4949bbc654e67fb6db684a0fbdb468ab
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-16 10:40:26 +0100

    Create 2-calcul_chai.js

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,24 @@
+function calculateNumber(type, a, b) {
+  // Round the input numbers
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+
+  // Check the type of operation
+  if (type === 'SUM') {
+    // Return the sum of the rounded numbers
+    return roundedA + roundedB;
+  } else if (type === 'SUBTRACT') {
+    // Return the difference of the rounded numbers
+    return roundedA - roundedB;
+  } else if (type === 'DIVIDE') {
+    // Check if the second number is zero
+    if (roundedB === 0) {
+      // Return 'Error' if attempting to divide by zero
+      return 'Error: Division by zero';
+    }
+    // Return the quotient of the rounded numbers
+    return roundedA / roundedB;
+  }
+}
+
+module.exports = calculateNumber;
